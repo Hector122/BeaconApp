fb61de9aa8d17f7b4e34118884760730a5b47747
Hector Castillo <hector_castillo@claro.com.do>
2016-01-12 16:36:09 -0400

    First commit, new application

---
+++ app/src/main/java/com/example/hectorcastillo/beacon/DashBoardActivty.java
@@ -0,0 +1,12 @@
+package com.example.hectorcastillo.beacon;
+
+import android.app.Activity;
+
+/**
+ * Created by hector castillo on 12/1/16.
+ */
+public class DashBoardActivty extends Activity {
+
+
+
+}
---
+++ app/src/main/java/com/example/hectorcastillo/beacon/MainActivity.java
@@ -0,0 +1,7 @@
+package com.example.hectorcastillo.beacon;
+
+/**
+ * Created by hector castillo on 12/1/16.
+ */
+public class MainActivity {
+}
